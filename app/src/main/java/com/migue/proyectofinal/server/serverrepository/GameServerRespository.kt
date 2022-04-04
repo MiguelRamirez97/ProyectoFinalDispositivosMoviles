005f95559d63020c3aed1a80622aef694fef38af
Tatiana Loaiza <ktatiana.garcia@udea.edu.co>
2022-04-04 06:11:51 -0500

    Partida local funcional

--- app/src/main/java/com/migue/proyectofinal/server/serverrepository/GameServerRespository.kt
+++ app/src/main/java/com/migue/proyectofinal/server/serverrepository/GameServerRespository.kt
@@ -1,6 +1,8 @@
 package com.migue.proyectofinal.server.serverrepository
 
 import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -12,9 +14,11 @@ import kotlinx.coroutines.withContext
 
 class GameServerRespository {
     val db = Firebase.firestore
-    suspend fun saveGame(uidPlayer: String?, namePlayer: String?) {
+
+    suspend fun saveQuickGame(uidPlayer: String?, namePlayer: String?) {
         val documentGame = db.collection("games").document()
-        val game = GameServer(id = documentGame.id, uidPlayer1 = uidPlayer, namePlayer1 = namePlayer)
+        val game =
+            GameServer(id = documentGame.id, uidPlayer1 = uidPlayer, namePlayer1 = namePlayer)
         db.collection("games")
             .document(documentGame.id)
             .set(game)
@@ -26,13 +30,20 @@ class GameServerRespository {
             }.await()
     }
 
-    suspend fun findGame(): QuerySnapshot? {
+    suspend fun findSyncQuickGame(): QuerySnapshot? {
         return withContext(Dispatchers.IO) {
             db.collection("games")
                 .whereEqualTo("namePlayer2", null)
                 .get()
                 .await()
         }
+    }
+
+    suspend fun findCurrentGame(idGame: String): QuerySnapshot? {
+        return db.collection("games")
+            .whereEqualTo("id", idGame)
+            .get()
+            .await()
     }
 
     suspend fun updateGame(gameServer: GameServer, playerServer: PlayerServer?) {
@@ -48,6 +59,46 @@ class GameServerRespository {
         return withContext(Dispatchers.IO) {
             db.collection("games")
                 .whereEqualTo("id", id)
+                .get()
+                .await()
+        }
+    }
+
+    fun saveLocalGame(
+        namePlayerOne: String,
+        uidPlayerOne: String,
+        namePlayertwo: String,
+        contadorPlayerOne: Int,
+        contadorPlayerTwo: Int,
+        winner: String
+    ) {
+        val documentGame = db.collection("games").document()
+        val game = GameServer(
+            id = documentGame.id,
+            uidPlayer1 = uidPlayerOne,
+            namePlayer1 = namePlayerOne,
+            namePlayer2 = namePlayertwo,
+            scorePlayer1 = contadorPlayerOne,
+            scorePlayer2 = contadorPlayerTwo,
+            winner = winner,
+            typeGame = "Juego local"
+        )
+        db.collection("games")
+            .document(documentGame.id)
+            .set(game)
+            .addOnSuccessListener {
+                Log.d("Game", "Juego creado.")
+            }
+            .addOnFailureListener { e ->
+                Log.w("Game", "Error creando el juego", e)
+            }
+    }
+
+    suspend fun findGameAgain(uid : String): QuerySnapshot? {
+        return withContext(Dispatchers.IO) {
+            db.collection("games")
+                .whereEqualTo("uidPlayer1", uid)
+                .whereEqualTo("winner",null)
                 .get()
                 .await()
         }
